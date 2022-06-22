833db46f9579b46bdd3fe3ff527f4f4203ff1244
Justin3go <2197432182@qq.com>
2022-06-22 12:46:54 +0800

    chore:none

--- Reference/nft/scripts/simple_collectible/deploy_simple.py
+++ Reference/nft/scripts/simple_collectible/deploy_simple.py
@@ -2,6 +2,7 @@
 from brownie import SimpleCollectible, accounts, network, config
 from scripts.helpful_scripts import get_publish_source
 
+
 def main():
     dev = accounts.add(config["wallets"]["from_key"])
     print(network.show_active())
