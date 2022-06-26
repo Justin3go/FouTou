41003e222f8edccd0688e3f4f71039f84f1f498a
Justin3go <2197432182@qq.com>
2022-06-26 14:07:23 +0800

    feat:add array common methods

---
+++ Contracts/utils/ArrayLib.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+library ArrayLib{
+    // 在给定数组中找到对应元素的索引
+    function find(uint[] storage arr, uint v) internal view returns (uint) {
+        for(uint i = 0; i < arr.length; i++){
+          if(arr[i] == v){
+            return i;
+          }
+        }
+        revert("not found");
+    }
+    // 根据索引删除元素（不保证顺序）
+    function removeByIndex(uint[] storage arr, uint _index) internal{
+      require(_index < arr.length, "index out of bound");
+      arr[_index] = arr[arr.length-1];
+      arr.pop();
+    }
+    // 根据uint值是否相等删除第一个相等的元素（不保证顺序）
+    function removeByVal(uint[] storage arr, uint val) internal {
+      uint _index = find(arr, val);
+      removeByIndex(arr, _index);
+    }
+}
