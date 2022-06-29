f15d75dfb5e947876b872876ca10d2574f476585
Justin3go <2197432182@qq.com>
2022-06-29 16:29:49 +0800

    feat:add auth api

--- foutouApp/src/api/etherApi.ts
+++ foutouApp/src/api/etherApi.ts
@@ -24,9 +24,67 @@ function initContract() {
 }
 
 export const Auth = {
-	async verifyRole(role: string, account: string): Promise<boolean> {
+	/**
+	 * 验证某个账户是否属于某个角色(SUPER_ADMIN, ADMIN, USER)
+	 * @param role bytes32 哈希了的role字符，以下三选一
+	 * SUPER_ADMIN=>0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
+	 * ADMIN=>0xd980155b32cf66e6af51e0972d64b9d5efe0e6f237dfaa4bdc83f990dd79e9c8
+	 * USER=>0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96
+	 * @param account address
+	 */
+	verifyRole(role: string, account: string): Promise<boolean> {
 		const { contract } = initContract();
-    const res = await contract.roles(role, account);
-    return res; // todo 测试
+		return contract.roles(role, account);
+	},
+
+	getADMIN_NUM(): Promise<number> {
+		const { contract } = initContract();
+		return contract.ADMIN_NUM();
+	},
+	getREQUIRED_ADMIN(): Promise<number> {
+		const { contract } = initContract();
+		return contract.REQUIRED_ADMIN();
+	},
+	getREQUIRED_REPOERTER(): Promise<number> {
+		const { contract } = initContract();
+		return contract.REQUIRED_REPOERTER();
+	},
+	getREQUIRED_FANS(): Promise<number> {
+		const { contract } = initContract();
+		return contract.REQUIRED_FANS();
+	},
+	getIS_TEST_VERSION(): Promise<boolean> {
+		const { contract } = initContract();
+		return contract.IS_TEST_VERSION();
+	},
+	// setConfig(){},
+	// transferSUPER_ADMIN(){},
+	/**
+	 * @param account address
+	 */
+	USER2ADMIN(account: string): Promise<void> {
+		const { contract } = initContract();
+		return contract.USER2ADMIN(account);
+	},
+	/**
+	 * @param account address
+	 */
+	ADMIN2USER(account: string): Promise<void> {
+		const { contract } = initContract();
+		return contract.ADMIN2USER(account);
+	},
+	/**
+	 * @param account address
+	 */
+	registerByAdmin(account: string): Promise<void> {
+		const { contract } = initContract();
+		return contract.registerByAdmin(account);
+	},
+	/**
+	 * @param account address
+	 */
+	publicRegister(account: string): Promise<void> {
+		const { contract } = initContract();
+		return contract.publicRegister(account);
 	},
 };
