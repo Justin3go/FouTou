e90a427a0bab13c525f63806373fe8eeb4fcb7ad
Justin3go <2197432182@qq.com>
2022-06-30 11:07:52 +0800

    chore:按需引入element-plus

--- foutouApp/src/App.vue
+++ foutouApp/src/App.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { ElConfigProvider } from "element-plus";
+import { ref } from "vue";
+
+const size = ref("small" as any);
+const zIndex = ref(3000);
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<el-config-provider :size="size" :z-index="zIndex">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	</el-config-provider>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
--- foutouApp/vite.config.ts
+++ foutouApp/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+// element-plus
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +14,13 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "src"),
 		},
 	},
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()],
+		}),
+	],
 });
