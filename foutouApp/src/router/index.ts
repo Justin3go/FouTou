d27550a64f15254791b2ed55f5f26c61243ccb69
Justin3go <2197432182@qq.com>
2022-07-04 21:11:32 +0800

    feat:添加账户未授权的路由守卫

--- foutouApp/src/router/index.ts
+++ foutouApp/src/router/index.ts
@@ -1,32 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Start from '../pages/Start.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Start from "../pages/Start.vue";
+import { useEtherStore } from "@/store/etherStore";
 
+let etherStore: any = null;
 const routes = [
-  {
-    path: '/',
-    name: 'start',
-    component: Start
-  },
-  {
-    path: '/browse',
-    name: 'browse',
-    component: () => import('../pages/Browse.vue')
-  },
-  {
-    path: '/personal',
-    name: 'personal',
-    component: () => import('../pages/Personal.vue')
-  },
-  {
-    path: '/create',
-    name: 'create',
-    component: () => import('../pages/Create.vue')
-  }
-]
+	{
+		path: "/",
+		name: "start",
+		component: Start,
+	},
+	{
+		path: "/browse",
+		name: "browse",
+		component: () => import("../pages/Browse.vue"),
+	},
+	{
+		path: "/personal",
+		name: "personal",
+		component: () => import("../pages/Personal.vue"),
+	},
+	{
+		path: "/create",
+		name: "create",
+		component: () => import("../pages/Create.vue"),
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+});
 
-export default router
+router.beforeEach((to, from) => {
+	if (etherStore === null) {
+		etherStore = useEtherStore();
+	}
+  if(!etherStore.account){
+    ElMessage({
+			showClose: true,
+			message: "未获取账户信息，请到【其他>连接钱包】中操作",
+			type: "error",
+		});
+  }
+	console.log(etherStore.account);
+});
+
+export default router;
