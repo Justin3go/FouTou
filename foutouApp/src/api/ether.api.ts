616a953c6e490846f556b90420ad8f081cb4290b
郑辉 <zhenghui.3go@bytedance.com>
2022-07-26 10:12:18 +0800

    feat: 完成合约基础api

--- foutouApp/src/api/ether.api.ts
+++ foutouApp/src/api/ether.api.ts
@@ -40,6 +40,9 @@ export class BaseAuth {
 	verifyRole(role: string, account: string): Promise<boolean> {
 		return this.contract.roles(role, account);
 	}
+	getWITHDRAW_OWNER(): Promise<string>{
+		return this.contract.WITHDRAW_OWNER();
+	}
 	/** 获取预计管理员的数量 */
 	getADMIN_NUM(): Promise<number> {
 		return this.contract.ADMIN_NUM();
@@ -49,6 +52,12 @@ export class BaseAuth {
 	}
 	getREQUIRED_REPOERTER(): Promise<number> {
 		return this.contract.REQUIRED_REPOERTER();
+	}
+	getREPORT_ETHER():Promise<number>{
+		return this.contract.REPORT_ETHER();
+	}
+	getREPORT_FEEDBACK():Promise<number>{
+		return this.contract.REPORT_FEEDBACK();
 	}
 	getREQUIRED_FANS(): Promise<number> {
 		return this.contract.REQUIRED_FANS();
@@ -83,6 +92,10 @@ export class BaseAuth {
 	publicRegister(account: string): Promise<void> {
 		return this.contract.publicRegister(account);
 	}
+	getBalance():Promise<number>{
+		return this.contract.getBalance();
+	}
+	// withdraw(){}
 	// 3. event
 	// 事件相关，参数名字虽然与合约事件参数名相同，但这里代表的是该参数的topic集合，并且有indexed的这里才有参数
 	E_transferAdmin(account: TopicAccount = null, grantOrRevoke: TopicBool = null) {
@@ -97,6 +110,7 @@ export class BaseAuth {
 		const eventFilter = this.contract.filters.Register(admin, account);
 		return this.contract.queryFilter(eventFilter);
 	}
+	// E_withdraw(){}
 }
 
 export class BasePhoto {
@@ -393,344 +407,11 @@ export class BaseCommunity {
 		return this.contract.queryFilter(eventFilter);
 	}
 }
-// todo 后续还要把刚才新增的几个方法加入
-
-export const Auth_ = {
-	/**
-	 * 验证某个账户是否属于某个角色(SUPER_ADMIN, ADMIN, USER)
-	 * @param role bytes32 哈希了的role字符，以下三选一
-	 * SUPER_ADMIN=>0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
-	 * ADMIN=>0xd980155b32cf66e6af51e0972d64b9d5efe0e6f237dfaa4bdc83f990dd79e9c8
-	 * USER=>0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96
-	 * @param account address
-	 */
-	verifyRole(role: string, account: string): Promise<boolean> {
-		const { contract } = initContract();
-		return contract.roles(role, account);
-	},
-	getRigister(admin: string) {
-		const { contract } = initContract();
-		let eventFilter = contract.filters.Register(admin);
-		return contract.queryFilter(eventFilter);
-	},
-
-	getADMIN_NUM(): Promise<number> {
-		const { contract } = initContract();
-		return contract.ADMIN_NUM();
-	},
-	getREQUIRED_ADMIN(): Promise<number> {
-		const { contract } = initContract();
-		return contract.REQUIRED_ADMIN();
-	},
-	getREQUIRED_REPOERTER(): Promise<number> {
-		const { contract } = initContract();
-		return contract.REQUIRED_REPOERTER();
-	},
-	getREQUIRED_FANS(): Promise<number> {
-		const { contract } = initContract();
-		return contract.REQUIRED_FANS();
-	},
-	getIS_TEST_VERSION(): Promise<boolean> {
-		const { contract } = initContract();
-		return contract.IS_TEST_VERSION();
-	},
-	// setConfig(){},
-	// transferSUPER_ADMIN(){},
-	/**
-	 * @param account address
-	 */
-	USER2ADMIN(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.USER2ADMIN(account);
-	},
-	/**
-	 * @param account address
-	 */
-	ADMIN2USER(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.ADMIN2USER(account);
-	},
-	/**
-	 * @param account address
-	 */
-	registerByAdmin(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.registerByAdmin(account);
-	},
-	/**
-	 * @param account address
-	 */
-	publicRegister(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.publicRegister(account);
-	},
-};
-export const Photo_ = {
-	/**
-	 * 根据tokenID查询FT所有信息
-	 * @param tokenID uint256
-	 * @return json  // todo
-	 */
-	getFTinfo(tokenID: number): Promise<string> {
-		const { contract } = initContract();
-		return contract.FTMap(tokenID);
-	},
-	/**
-	 * 根据tokenID查询该FT下所有购买者
-	 * @param tokenID uint256
-	 * @return address[]
-	 */
-	getBuyers(tokenID: number): EventFilter {
-		// todo 接下来就是试试这个事件是否能查成功，还有per_owned——FT
-		const { contract } = initContract();
-		return contract.filters.BuyFT(null, null, tokenID);
-	},
-	/**
-	 * 修改价格
-	 * @param tokenID uint256
-	 * @param newPrice uint256
-	 */
-	alertPrice(tokenID: number, newPrice: number): Promise<void> {
-		const { contract } = initContract();
-		return contract.alertPrice(tokenID, newPrice);
-	},
-	alertDescription(tokenID: number, newDes: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.alertDescription(tokenID, newDes);
-	},
-};
-
-export const Person_ = {
-	/**
-	 * 获取用户信息
-	 * @param account address
-	 * @return string(json)
-	 */
-	getPER_items(account: string): Promise<string> {
-		const { contract } = initContract();
-		return contract.PER_items(account);
-	},
-	/**
-	 * 获取广告等级和广告图片链接
-	 * @param account address
-	 * @return 1$https://www.example.com
-	 */
-	getPER_ad(account: string): Promise<string> {
-		const { contract } = initContract();
-		return contract.PER_ad(account);
-	},
-	/**
-	 * 获取个人信用值
-	 * @param account address
-	 */
-	getPER_credit(account: string): Promise<number> {
-		const { contract } = initContract();
-		return contract.PER_credit(account);
-	},
-	/**
-	 * 获取个人拥有的FT
-	 * @param account address
-	 * @return [tokenID, tokenID, ...]
-	 */
-	getPER_ownedFT(account: string): EventFilter {
-		const { contract } = initContract();
-		// return contract.getPER_ownedFT(account);
-		return contract.filters.AddFT(null);
-	},
-	/**
-	 * 获取个人购买的FT
-	 * @param account address
-	 * @return [tokenID, tokenID, ...]
-	 */
-	getPER_boughtFT(account: string): Promise<Array<number>> {
-		const { contract } = initContract();
-		return contract.getPER_boughtFT(account);
-	},
-	/**
-	 * 获取个人拥有的粉丝
-	 * @param account address
-	 * @return [address, address, ...]
-	 */
-	getPER_fans(account: string): Promise<Array<string>> {
-		const { contract } = initContract();
-		return contract.getPER_fans(account);
-	},
-	/**
-	 * 获取个人拥有的粉丝
-	 * @param account address
-	 * @return [address, address, ...]
-	 */
-	getPER_follow(account: string): Promise<Array<string>> {
-		const { contract } = initContract();
-		return contract.getPER_follow(account);
-	},
-	/**
-	 * 自己修改自己的信息
-	 * @param items string(json)
-	 */
-	alertPER_items(items: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.alertPER_items(items);
-	},
-	/**
-	 * 修改自己的广告等级和广告图片链接
-	 * @param ad <广告等级>$<广告链接>
-	 */
-	alertPER_ad(ad: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.alertPER_ad(ad);
-	},
-	/**
-	 * 降低某位用户的信用分，每位管理员对同一位用户只能操作一次
-	 * @param account address
-	 */
-	reducePER_credit(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.reducePER_credit(account);
-	},
-	/**
-	 * 撤销降低，需要之前操作过
-	 * @param account address
-	 */
-	revokeReduce(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.revokeReduce(account);
-	},
-};
-// todo internal->private
-export const Copyright_ = {
-	/**
-	 * 获取举报人数达到，举报消息提交的时间
-	 * @param tokenID uint256
-	 * @return 时间
-	 */
-	getMessageTime(tokenID: number): Promise<number> {
-		const { contract } = initContract();
-		return contract.messageTime(tokenID);
-	},
-	/**
-	 * 获取某消息同意的管理员人数
-	 * @param tokenID uint256
-	 */
-	getApproveCount(tokenID: number): Promise<number> {
-		const { contract } = initContract();
-		return contract.approveCount(tokenID);
-	},
-	/**
-	 * 获取某管理员处理的所有消息
-	 * @param account address
-	 * @return [tokenID, tokenID, ...]
-	 */
-	getProcessed(account: string): Promise<Array<number>> {
-		const { contract } = initContract();
-		return contract.getProcessed(account);
-	},
-	/**
-	 * 获取某FT的所有举报人
-	 * @param tokenID uint256
-	 * @return [address, address, ...]
-	 */
-	getMES_reporters(tokenID: number): Promise<Array<string>> {
-		const { contract } = initContract();
-		return contract.getMES_reporters(tokenID);
-	},
-	/**
-	 * 获取所有已经提交举报的tokenID
-	 * @return [tokenID, tokenID, ...]
-	 */
-	getReportedTokenID(): Promise<Array<number>> {
-		const { contract } = initContract();
-		return contract.getReportedTokenID();
-	},
-	/**
-	 * 举报某件FT
-	 * @param tokenID uint256
-	 */
-	report(tokenID: number): Promise<void> {
-		const { contract } = initContract();
-		return contract.report(tokenID);
-	},
-	/**
-	 * 管理员同意某举报FT的消息
-	 * @param tokenID uint256
-	 */
-	approve(tokenID: number): Promise<void> {
-		const { contract } = initContract();
-		return contract.approve(tokenID);
-	},
-	/**
-	 * 管理员拒绝某举报FT的消息
-	 * @param tokenID uint256
-	 */
-	reject(tokenID: number): Promise<void> {
-		const { contract } = initContract();
-		return contract.reject(tokenID);
-	},
-	/**
-	 * 管理员忽略某举报FT的消息
-	 * @param tokenID uint256
-	 */
-	ignore(tokenID: number): Promise<void> {
-		const { contract } = initContract();
-		return contract.ignore(tokenID);
-	},
-	/**
-	 * 购买某件FT，调用时传入其他人地址就是为别人购买，
-	 * @param tokenID uint256
-	 * @param account address
-	 */
-	buyFT(tokenID: number, account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.buyFT(tokenID, account);
-	},
-	/**
-	 * 铸造FT
-	 * @param tokenURI 图片链接，需要ipfs
-	 * @param owner address, 可以为自己和他人打造
-	 * @param price uint256
-	 * @param description string
-	 */
-	addFT(tokenURI: string, owner: string, price: number, description: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.addFT(tokenURI, owner, price, description);
-	},
-};
-
-export const Community_ = {
-	/**
-	 * 关注某位博主
-	 * @param account address 博主的地址
-	 */
-	follow(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.follow(account);
-	},
-	/**
-	 * 取消关注某位博主，由于要遍历，消耗的gas可能会很多
-	 * @param account address 博主的地址
-	 */
-	cancelFollow(account: string): Promise<void> {
-		const { contract } = initContract();
-		return contract.cancelFollow(account);
-	},
-};
-// todo 查出自己关注人的动态，分页展示
-export const Event_ = {
-	/**
-	 * 分页查询自己关注的所有博主的动态
-	 * @param account address
-	 * @param page 第几页
-	 * @param size 每页数量
-	 * @return [tokenID, tokenID, ...]
-	 */
-	getAll(account: string, page: number, size: number): Promise<Array<number>> {
-		return Promise.resolve([1]); // todo
-	},
-	/**
-	 * 订阅自己关注的博主之后发布的图片，博主每次发布图片，就将其信息存储到本地(后续可以加一个清除本地缓存的功能)
-	 */
-	subscribe(account: string) {
-		const { contract } = initContract();
-	},
-	unSubscribe(account: string) {},
-};
+// todo 封装基础api
+export class Auth extends BaseAuth {
+	
+}
+export class Photo extends BasePhoto{}
+export class Person extends BasePerson{}
+export class Copyright extends BaseCopyright{}
+export class Community extends BaseCommunity{}
